e4078d3112a36c9c0e52e84d52983320aef2b399
Sofia Gonzalez <sofi7zubieta@gmail.com>
2023-05-28 02:03:46 -0500

    sending test email

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,16 @@
 import User from "../models/user";
 import { hashPassword, comparePassword} from "../utils/auth";
 import jwt from "jsonwebtoken";
+import AWS from 'aws-sdk'
+
+const awsConfig={
+  accessKeyId:  process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey:  process.env.AWS_SECRET_ACCESS_KEY_ID,
+  region: process.env.AWS_REGION,
+  apiVersion: process.env.AWS_API_VERSION,
+};
+
+const SES= new AWS.SES(awsConfig);
 
 export const register = async (req, res) => {
   try{ // codigo que puede manejar errores
@@ -89,8 +99,43 @@ export const currentUser = async (req, res) => {
   }
 };
 
-export const sendTestEmail= async (req,res) =>{
-  console.log('send email using SES');
-  res.json({ ok: true });
 
-}
+export const sendTestEmail = async (req, res) => {
+  // console.log("send email using SES");
+  // res.json({ ok: true });
+  const params = {
+    Source: process.env.EMAIL_FROM,
+    Destination: {
+      ToAddresses: ["sofi7zubieta@gmail.com"],
+    },
+    ReplyToAddresses: [process.env.EMAIL_FROM],
+    Message: {
+      Body: {
+        Html: {
+          Charset: "UTF-8",
+          Data: `
+            <html>
+            <h1>Enlace de restablecimiento de contraseña</h1>
+            <p> Por favor usa el siguiente link para restablecer tu contraseña: </p>
+            </html>
+          `,
+        },
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: "Password reset link",
+      },
+    },
+  };
+
+  const emailSent = SES.sendEmail(params).promise();
+
+  emailSent
+    .then((data) => {
+      console.log(data);
+      res.json({ ok: true });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
